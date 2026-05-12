a6be445c922e4571787b2279ca98f8c6bef230fd
Fandy Barestu <fandy@softwareseni.com>
2026-05-12 14:22:01 +0700

    feat(storage): make S3 config optional to allow deploy without media storage

--- src/modules/storage/storage.service.ts
+++ src/modules/storage/storage.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, ServiceUnavailableException } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import {
   GetObjectCommand,
@@ -9,29 +9,40 @@ import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 
 @Injectable()
 export class StorageService {
-  private readonly s3: S3Client;
-  private readonly bucket: string;
+  private readonly s3: S3Client | null = null;
+  private readonly bucket: string | null = null;
 
   constructor(private readonly configService: ConfigService) {
+    const region = this.configService.get<string>('AWS_REGION');
     const endpoint = this.configService.get<string>('AWS_ENDPOINT');
-    this.s3 = new S3Client({
-      region: this.configService.getOrThrow<string>('AWS_REGION'),
-      ...(endpoint && { endpoint }),
-      credentials: {
-        accessKeyId: this.configService.getOrThrow<string>('AWS_ACCESS_KEY_ID'),
-        secretAccessKey: this.configService.getOrThrow<string>(
-          'AWS_SECRET_ACCESS_KEY',
-        ),
-      },
-      forcePathStyle: !!endpoint,
-    });
-    this.bucket = this.configService.getOrThrow<string>('AWS_BUCKET');
+    const accessKeyId = this.configService.get<string>('AWS_ACCESS_KEY_ID');
+    const secretAccessKey = this.configService.get<string>(
+      'AWS_SECRET_ACCESS_KEY',
+    );
+    const bucket = this.configService.get<string>('AWS_BUCKET');
+
+    if (region && accessKeyId && secretAccessKey && bucket) {
+      this.s3 = new S3Client({
+        region,
+        ...(endpoint && { endpoint }),
+        credentials: { accessKeyId, secretAccessKey },
+        forcePathStyle: !!endpoint,
+      });
+      this.bucket = bucket;
+    }
+  }
+
+  private assertConfigured(): void {
+    if (!this.s3 || !this.bucket) {
+      throw new ServiceUnavailableException('Storage not configured');
+    }
   }
 
   async upload(file: Express.Multer.File, key: string): Promise<string> {
-    await this.s3.send(
+    this.assertConfigured();
+    await this.s3!.send(
       new PutObjectCommand({
-        Bucket: this.bucket,
+        Bucket: this.bucket!,
         Key: key,
         Body: file.buffer,
         ContentType: file.mimetype,
@@ -41,9 +52,10 @@ export class StorageService {
   }
 
   async getSignedUrl(key: string, expiresIn = 3600): Promise<string> {
+    this.assertConfigured();
     return getSignedUrl(
-      this.s3,
-      new GetObjectCommand({ Bucket: this.bucket, Key: key }),
+      this.s3!,
+      new GetObjectCommand({ Bucket: this.bucket!, Key: key }),
       { expiresIn },
     );
   }
